3bcd2918dc818180963b159691e0e8b159c4fb6b
Agata <37178984+agatazbikowska@users.noreply.github.com>
2020-03-03 19:04:22 +0100

    Delete strona_główna.R

--- strona_główna.R
+++
@@ -1,6 +0,0 @@
-## I Konferencja Psychologiczna Gdańskiego Uniwersytetu Medycznego
-
-#### SKN Psychologii Gdańskiego Uniwersytetu Medycznego ma przyjemność zaprosić Państwa I Konferencję Psychologiczną Gdańskiego Uniwersytetu Medycznego.
-
-#### Konferencja to wydarzenie adresowane do studentów psychologii, psychologii zdrowia oraz wszystkich innych zainteresowanych tematyką.
-
